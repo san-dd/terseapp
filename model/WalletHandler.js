39c31cb134f54df856f1b423894e90657b1c53d2
san-dd <54465038+san-dd@users.noreply.github.com>
2020-07-20 16:00:18 +0530

    Update WalletHandler.js

--- model/WalletHandler.js
+++ model/WalletHandler.js
@@ -4,7 +4,7 @@ class WalletHandler {
     create_wallet(data, callback) {
         unirest
             .post('https://api.streetcred.id/custodian/v1/api/wallets')
-            .headers({ 'accept': 'text/plain', 'Content-Type': 'application/json', 'Authorization': 'bearer IQiLu-***********',', 'X-Streetcred-Subscription-Key': '***********',' })
+            .headers({ 'accept': 'text/plain', 'Content-Type': 'application/json', 'Authorization': 'bearer IQiLu-***********', 'X-Streetcred-Subscription-Key': '***********' })
             .send({ "ownerName": `${data.ownerName}` })
             .then((response) => {
                 console.log(response.body)
@@ -16,7 +16,7 @@ class WalletHandler {
     wallet_list(data, callback) {
         unirest
             .get('https://api.streetcred.id/custodian/v1/api/wallets')
-            .headers({ 'accept': 'text/plain', 'Content-Type': 'application/json', 'Authorization': 'bearer ***********', 'X-Streetcred-Subscription-Key': '***********',' })
+            .headers({ 'accept': 'text/plain', 'Content-Type': 'application/json', 'Authorization': 'bearer ***********', 'X-Streetcred-Subscription-Key': '***********' })
             .send()
             .then((response) => {
                 console.log(response.body)
@@ -27,7 +27,7 @@ class WalletHandler {
     delete_wallet(data, callback) {
         unirest
             .delete('https://api.streetcred.id/wallet/v1/api/wallets?walletId=' + data.walletid)
-            .headers({ 'accept': 'text/plain', 'Content-Type': 'application/json', 'Authorization': 'bearer IQiLu-***********',', 'X-Streetcred-Subscription-Key': '***********',' })
+            .headers({ 'accept': 'text/plain', 'Content-Type': 'application/json', 'Authorization': 'bearer IQiLu-***********', 'X-Streetcred-Subscription-Key': '***********' })
             .send()
             .then((response) => {
                 if (response.body) {
